1687ac810e5af72fa1c2f87133e23550ff41368f
sumeriancore <SAusianikau@ibagroup.eu>
2022-01-23 23:44:55 +0300

    stefan/HerokuTest-1 Hello world with Spring Boot + Docker + Heroku

---
+++ src/main/java/com/heroku/test/deploy/SpringDockerHeroku/SpringDockerHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.test.deploy.SpringDockerHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerHerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heroku/test/deploy/SpringDockerHeroku/SpringDockerHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.test.deploy.SpringDockerHeroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
